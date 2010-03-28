1893e48a392a747f2b8a4cacd3405dd0f10a1cb1
Jason May <jason.a.may@gmail.com>
2010-03-28 16:28:52 -0400

    begin switching to IO::Multiplex

--- lib/IO/Multiplex/Intermediary.pm
+++ lib/IO/Multiplex/Intermediary.pm
@@ -1,29 +1,71 @@
 #!/usr/bin/env perl
 package IO::Multiplex::Intermediary;
-use MooseX::POE;
+use Moose;
 use namespace::autoclean;
 
-use JSON;
+use List::Util      qw(first);
 use List::MoreUtils qw(any);
-use Scalar::Util qw(reftype);
+use Scalar::Util    qw(reftype);
+use IO::Socket;
+use IO::Multiplex;
+use Data::UUID;
+use JSON;
 
-use POE qw(
-    Wheel::SocketFactory
-    Component::Server::TCP
-    Wheel::ReadWrite
-    Filter::Stream
+has mux => (
+    is         => 'ro',
+    isa        => 'IO::Multiplex',
+    lazy_build => 1,
 );
 
-has external_sockets => (
-    is  => 'rw',
-    isa => 'POE::Wheel::SocketFactory',
-);
-
-has rw_set => (
-    is => 'rw',
-    isa => 'HashRef[Int]',
+has filehandles => (
+    is      => 'ro',
+    isa     => 'HashRef[IO::Socket::INET]',
     default => sub { +{} },
 );
+
+has listener => (
+    is         => 'ro',
+    isa        => 'IO::Socket::INET',
+    lazy_build => 1,
+);
+
+sub _build_listener {
+    my $self   = shift;
+    warn "foooioo";
+    my $socket = IO::Socket::INET->new(
+        LocalPort => $self->external_port,
+        Proto     => 'tcp',
+        Listen    => 5,
+        Reuse     => 1,
+    ) or die $!;
+    warn $socket;
+    return $socket;
+}
+
+sub id_lookup {
+    my $self = shift;
+    my $fh   = shift;
+
+    return first { $self->filehandles->{$_} == $fh }
+           keys %{ $self->filehandles };
+}
+
+sub _build_mux {
+    my $self   = shift;
+    my $mux    = IO::Multiplex->new;
+
+    my $socket = IO::Socket::INET->new(
+        LocalPort => $self->external_port,
+        Proto     => 'tcp',
+        Listen    => 5,
+        Reuse     => 1,
+    );
+
+    $mux->listen($socket);
+    $mux->set_callback_object($self);
+
+    return $mux;
+}
 
 has external_port => (
     is  => 'ro',
@@ -31,18 +73,32 @@ has external_port => (
     default => 6715
 );
 
-has controller_socket => (
-    is      => 'rw',
-    isa     => 'POE::Wheel::ReadWrite',
+has client_handle => (
+    is         => 'rw',
+    isa        => 'IO::Socket::INET',
+    lazy_build => 1,
 );
 
-has controller_port => (
+sub _build_client_handle {
+    my $self = shift;
+
+    my $socket = IO::Socket::INET->new(
+        LocalPort => $self->client_port,
+        Proto     => 'tcp',
+        Listen    => 5,
+    );
+
+    $self->mux->listen($socket);
+    return $socket;
+}
+
+has client_port => (
     is  => 'ro',
     isa => 'Int',
     default => 9000
 );
 
-has controller_connected => (
+has client_connected => (
     is  => 'rw',
     isa => 'Bool',
     default => 0
@@ -54,34 +110,13 @@ has socket_info => (
     default => sub { +{} },
 );
 
-sub _start {
-    my ($self) = @_;
-    $self->external_sockets(
-        POE::Wheel::SocketFactory->new(
-            BindPort     => $self->external_port,
-            SuccessEvent => 'client_accept',
-            FailureEvent => 'server_error',
-            Reuse        => 'yes',
-        )
-    );
-
-    POE::Component::Server::TCP->new(
-        Port               => $self->controller_port,
-        ClientConnected    => sub { $self->_controller_client_accept(@_) },
-        ClientDisconnected => sub { $self->_controller_server_error(@_)  },
-        ClientInput        => sub { $self->_controller_client_input(@_)  },
-    );
-}
-
 #TODO send backup info
-sub _controller_client_accept {
+sub client_connection {
     my $self = shift;
 
-    $self->controller_socket($_[HEAP]->{client});
-
-    if ( scalar(%{$self->rw_set}) ) {
-        foreach my $id (keys %{ $self->rw_set }) {
-            $self->send_to_controller(
+    if ( scalar(%{$self->filehandles}) ) {
+        foreach my $id (keys %{ $self->filehandles }) {
+            $self->send_to_client(
                 {
                     param => 'connect',
                     data  => {
@@ -93,49 +128,60 @@ sub _controller_client_accept {
     }
 }
 
-sub _client_accept {
-    my ($self) = @_;
-    my $socket = $_[ARG0];
-    my $rw = POE::Wheel::ReadWrite->new(
-        Handle     => $socket,
-        Driver     => POE::Driver::SysRW->new,
-        Filter     => POE::Filter::Stream->new,
-        InputEvent => 'client_input',
-        ErrorEvent => 'client_error',
-    );
+{
+    my $du = Data::UUID->new;
+    sub mux_connection {
+        my $self = shift;
+        my $mux = shift;
+        my $fh = shift;
+        warn "foo";
 
-    my $wheel_id = $rw->ID;
-    $self->rw_set->{$wheel_id} = $rw;
-
-    $self->send_to_controller(
-        {
-            param => 'connect',
-            data  => {
-                id    => $wheel_id,
-            }
+        if ($fh == $self->client_handle) {
+            $self->client_connection;
+            return;
         }
-    );
+
+        my $id = $du->create_str;
+
+        $self->filehandles->{$id} = $fh;
+
+        $self->send_to_client(
+            {
+                param => 'connect',
+                data  => {
+                    id    => $id,
+                }
+            }
+        );
+    }
 }
 
-sub _client_input {
-    my ($self)             = @_;
-    my ($input, $wheel_id) = @_[ARG0, ARG1];
-    $input =~ s/[\r\n]*$//;
+sub mux_input {
+    my $self  = shift;
+    my $mux   = shift;
+    my $fh    = shift;
+    my $input = shift;
 
-    $self->send_to_controller(
+    warn "foo";
+    if ($fh == $self->client_handle) {
+        $self->client_input($$input);
+        return;
+    }
+
+    $self->send_to_client(
         {
             param => 'input',
             data => {
-                id    => $wheel_id,
-                value => $input,
+                id    => ,
+                value => $$input,
             }
         }
     );
 }
 
-sub _controller_client_input {
+sub client_input {
     my $self = shift;
-    my $input = $_[ARG0];
+    my $input = shift;
     chomp($input);
     my $json = eval { from_json($input) };
 
@@ -148,11 +194,11 @@ sub _controller_client_input {
         }
         else {
             last unless $json->{data}->{id};
-            last unless reftype($self->rw_set);
-            last unless $self->rw_set->{ $json->{data}->{id} };
+            last unless reftype($self->filehandles);
+            last unless $self->filehandles->{ $json->{data}->{id} };
 
             if ($json->{param} eq 'output') {
-                $self->rw_set->{ $json->{data}->{id} }->put( $json->{data}->{value} );
+                print { $self->filehandles->{ $json->{data}->{id} } } $json->{data}->{value};
                 if ($json->{updates}) {
                     foreach my $key  (%{ $json->{updates} }) {
                         my $value = $json->{updates}->{$key};
@@ -162,69 +208,60 @@ sub _controller_client_input {
             }
             elsif ($json->{param} eq 'disconnect') {
                 my $id = $json->{data}->{id};
-                $self->rw_set->{$id}->shutdown_output;
+                $self->filehandles->{$id}->shutdown_output;
             }
         }
     }
 
 }
 
-sub _client_error {
-    my ($self)   = @_;
-    my $wheel_id = $_[ARG3];
-    delete $self->rw_set->{$wheel_id};
-    $self->send_to_controller(
+sub mux_eof {
+    my $self = shift;
+    my $mux  = shift;
+    my $fh   = shift;
+
+    $self->send_to_client(
         {
             param => 'disconnect',
             data => {
-                id => $wheel_id,
+                id => $self->id_lookup($fh),
             }
         }
     );
 }
 
-#TODO clean shutdown etc
-sub _server_error {
-    #stub
+sub mux_timeout {
+    my $self = shift;
+    my $mux = shift;
 }
-
-sub _controller_server_error {
-    #stub
-}
-
 
 sub send {
     my $self = shift;
     my $id = shift;
     my $data = shift;
 
-    $self->rw_set->{$id}->put(to_json($data));
+    print { $self->filehandles->{$id} } to_json($data);
 }
 
-sub send_to_controller {
+sub send_to_client {
     my $self   = shift;
     my $data   = shift;
 
-    return unless defined $self->controller_socket;
-    $self->controller_socket->put(to_json($data));
+    return unless defined $self->client_handle;
+    print { $self->client_handle } to_json($data);
 }
-
 
 sub run {
     my $self = shift;
-    POE::Kernel->run();
+    $self->mux->loop;
 }
 
-event START                => \&_start;
-
-event client_accept => \&_client_accept;
-event server_error  => \&_server_error;
-event client_input  => \&_client_input;
-event client_error  => \&_client_error;
-
-#event controller_client_input   => \&_controller_client_input;
-##event controller_client_accept  => \&_controller_client_accept;
-#event controller_client_error   => \&_controller_client_error;
+sub DEMOLISH {
+    my $self = shift;
+    $self->listener->shutdown;
+    $self->mux->close($self->listener);
+    $self->mux->endloop;
+}
 
 __PACKAGE__->meta->make_immutable;
 
